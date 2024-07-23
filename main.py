e6b6164bcb0d7e727ac7ed88b2f250e494fc789d
javallecillo <javallecillo@uth.hn>
2024-07-23 16:50:11 -0600

    Martes 23/Julio/2024

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import pygame
 import constantes
 from personaje import Personaje
 
-# Variables
-jugador = Personaje(50,50)
-
 # Inicialisamos la libreria
 pygame.init()
 
@@ -14,31 +11,43 @@ ventana = pygame.display.set_mode((constantes.ANCHO_VENTANA, constantes.ALTO_VEN
 # Nombre de la ventana
 pygame.display.set_caption("Juego Progra. Avanzada")
 
+# Variables
+jugador = Personaje(50,50)
+
 #definir variables de movimiento del jugador
 mover_arriba = False
 mover_abajo = False
 mover_izquierda = False
 mover_derecha = False
 
+# controlar el movimiento del jugador
+reloj = pygame.time.Clock()
+
+
 # ciclo para mantener ventana
 run = True
 while run:
+
+    # velocidad de 60 fps
+    reloj.tick(constantes.FPS)
+    
+    ventana.fill(constantes.COLOR_BG)
 
     # calcular movimiento del jugador
     delta_x = 0
     delta_y = 0
 
     if mover_derecha == True:
-        delta_x = 5
+        delta_x = constantes.VELOCIDAD_PERSONAJE
 
     if mover_izquierda == True:
-        delta_x = -5
+        delta_x = -constantes.VELOCIDAD_PERSONAJE
 
     if mover_arriba == True:
-        delta_y = -5
+        delta_y = -constantes.VELOCIDAD_PERSONAJE
 
     if mover_abajo == True:
-        delta_y = 5
+        delta_y = constantes.VELOCIDAD_PERSONAJE
 
     # mover al jugar
     jugador.movimiento(delta_x, delta_y)
@@ -54,15 +63,25 @@ while run:
 
         # evento para mover el personaje
         if event.type == pygame.KEYDOWN:
-            if event.key ==  pygame.K_a:
+            if event.key ==  pygame.K_a or event.key == pygame.K_LEFT:
                 mover_izquierda = True
-            if event.key == pygame.K_d:
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                 mover_derecha = True
-            if event.key == pygame.K_w:
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
                 mover_arriba = True
-            if event.key == pygame.K_s:
+            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
                 mover_abajo = True
 
+        # evento para dejar de mover el personaje
+        if event.type == pygame.KEYUP:
+            if event.key ==  pygame.K_a or event.key == pygame.K_LEFT:
+                mover_izquierda = False
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                mover_derecha = False
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
+                mover_arriba = False
+            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                mover_abajo = False
 
     pygame.display.update()
 
